6a3b71744eac0c96aef8bcb9d5e4529978a29a51
Huang Chen's lab <135127832+ChenHuangMUST@users.noreply.github.com>
2023-06-06 19:47:10 +0800

    Update combat.rmbatch.R

--- ML/combat.rmbatch.R
+++ ML/combat.rmbatch.R
@@ -1,29 +1,32 @@
-library(FactoMineR)##没有请先安装
+library(FactoMineR)
 library(factoextra)
 library(sva)
 library(bladderbatch)
-#BiocManager::install("sva")
-library(sva)
 
-dat_3AD=apply(dd_3AD,2,as.numeric)
-row.names(dat_3AD)=row.names(dd_3AD)
-#去批次
-combat_edata <- ComBat(dat = dd_3AD, batch = batch_3AD[,2])
+# Convert data to numeric format and assign row names
+dat_3AD <- apply(dd_3AD, 2, as.numeric)
+row.names(dat_3AD) <- row.names(dd_3AD)
 
-#生成pca
-pre.pca <- PCA(t(dat_3AD),graph = FALSE)
-pre.pca.n <- PCA(t(combat_edata),graph = FALSE)
+# Remove batch effects using ComBat
+combat_edata <- ComBat(dat = dd_3AD, batch = batch_3AD[, 2])
 
-#绘图
-batch_3AD[,2]=c(rep("GSE63060",ncol(dd1)),rep("GSE140829",ncol(dd2)),rep("GSE63061",ncol(dd3)))
+# Generate PCA plots for pre-processed and post-processed data
+pre.pca <- PCA(t(dat_3AD), graph = FALSE)
+pre.pca.n <- PCA(t(combat_edata), graph = FALSE)
+
+# Create group labels for visualization
+batch_3AD[, 2] <- c(rep("GSE63060", ncol(dd1)), rep("GSE140829", ncol(dd2)), rep("GSE63061", ncol(dd3)))
+
+# Plot PCA for pre-processed data
 fviz_pca_ind(pre.pca,
-             geom= "point",
-             col.ind = bc_3[,2],
+             geom = "point",
+             col.ind = bc_3[, 2],
              addEllipses = TRUE,
-             legend.title="Group"  )
+             legend.title = "Group")
 
+# Plot PCA for post-processed data
 fviz_pca_ind(pre.pca.n,
-             geom= "point",
-             col.ind =bc_3[,2],
+             geom = "point",
+             col.ind = bc_3[, 2],
              addEllipses = TRUE,
-             legend.title="Group"  )
+             legend.title = "Group")
