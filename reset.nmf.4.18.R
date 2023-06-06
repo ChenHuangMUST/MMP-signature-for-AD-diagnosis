9bf9b6da4c5d4ed10b803ff6070950a9137ca396
Huang Chen's lab <135127832+ChenHuangMUST@users.noreply.github.com>
2023-06-06 19:56:44 +0800

    Update reset.nmf.4.18.R

--- reset.nmf.4.18.R
+++ reset.nmf.4.18.R
@@ -1,62 +1,47 @@
-load("imp_MPP.RData")
-load("D:/RProject/AD/AD_train.RData")
-load("D:/RProject/AD/NMF/afterbatch.marker.select.3.11")
-load("MPP.RData")
+# Load required data
+load(c("imp_MPP.RData", "D:/RProject/AD/AD_train.RData", 
+      "D:/RProject/AD/NMF/afterbatch.marker.select.3.11", "MPP.RData"))
+
 library(NMF)
-imp_MPP=imp_big
-dat=dat_rmbatch[which(as.numeric(imp_MPP[,5])<0.01),]
-index=which(pd[,2]==1)
-pd=pd[index,]
-index=intersect(rownames(pd),colnames(dat_rmbatch))
-dat=dat[,index]
-dat_e=exp(1)^dat
 
-x.e <- nmf(dat_e,2:10,nrun=10,seed=111111,method = 'brunet')
+# Subset and preprocess the data
+pd <- pd[pd[, 2] == 1, ]
+dat_rmbatch <- dat_rmbatch[rownames(imp_MPP[as.numeric(imp_MPP[, 5]) < 0.01, ]), ]
+dat <- dat_rmbatch[intersect(rownames(pd), colnames(dat_rmbatch)), ]
 
+# Perform NMF with different ranks
+expression_data <- as.matrix(dat, nrow(dat))
+nmf_results <- list()
+for (i in 2:10) {
+  nmf_results[[i]] <- nmf(expression_data, rank = i, nrun = 10, seed = 111111,
+                          method = 'brunet', normalize = TRUE)
+}
 
+# Define function to create different types of plots
+create_plot <- function(nmf_obj, plot_type) {
+  plot_file <- paste0(plot_type, "_rank", nmf_obj$rank, ".pdf")
+  pdf(file.path("output", plot_file), width = 7, height = 7, onefile = FALSE)
+  switch(plot_type,
+         "consensusmap" = consensusmap(nmf_obj, annRow = NA, annCol = NA,
+                                       main = "Consensus matrix", info = FALSE),
+         "coefmap" = coefmap(nmf_obj, annRow = NA, annCol = NA,
+                             main = "Metagene contributions in each sample",
+                             info = FALSE),
+         "basismap" = basismap(nmf_obj, annRow = NA, annCol = NA,
+                               main = "Metagenes", info = FALSE),
+         "plot" = plot(nmf_obj))
+  dev.off()
+}
 
-plot(x.e)
+# Create plots for each NMF result
+lapply(nmf_results, create_plot, plot_type = "consensusmap")
+lapply(nmf_results, create_plot, plot_type = "coefmap")
+lapply(nmf_results, create_plot, plot_type = "basismap")
+lapply(nmf_results, create_plot, plot_type = "plot")
 
-
-
-x.e.nmf.2=nmf(dat_e,2,nrun=10,seed=520,method = 'brunet') # run parameters,rank=3
-
-
-
-pdf('consensusmap_x.e.2.1.pdf',width = 7,height = 7,onefile = F)
-consensusmap(x.e.nmf.2,
-             annRow = NA,
-             annCol = NA,
-             main = "Consensus matrix",
-             info = FALSE)
-dev.off() 
-pdf('coefmap_rank2.pdf',width = 7,height = 7,onefile = F)
-coefmap(x.e.nmf.2,
-        annRow = NA,
-        annCol = NA,
-        main = "Metagene contributions in each sample",
-        info = FALSE)
-dev.off() 
-pdf('basismap_rank2.pdf',width = 7,height = 7,onefile = F)
-#作用：每一行显示主导的基底组分，即每一行有最高负载的基底组分。
-basismap(x.e.nmf.2,
-         annRow = NA,
-         annCol = NA,
-         main = "Metagenes",
-         info = FALSE)
-dev.off() 
-pdf('plotrank.pdf',width = 7,height = 7,onefile = F)
-plot(x.e)
-dev.off()
-
-summary(x.e)
-group <- predict(x.e.nmf.2)
-group <- as.data.frame(group)
-group$group <- paste0('Cluster',group$group)
-group$sample <- rownames(group)
-group<- group[order(group$group),]
-table(group$group)
-head(group)
-save(group,file="clu2-reset.RData")
-save.image()
-load(".RData")
+# Save clustering results
+cluster_labels <- predict(nmf_results[[2]])
+cluster_labels <- data.frame(cluster = paste0("Cluster", cluster_labels),
+                             sample = rownames(cluster_labels))
+cluster_labels <- cluster_labels[order(cluster_labels$group), ]
+save(cluster_labels, file = "output/clu2-reset.RData")
